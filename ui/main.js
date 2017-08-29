dc2f79f7770bf67a8ce50c43c407af5ca093c69e
Ashwani Gupta <ashwani99@users.noreply.github.com>
2017-08-29 21:36:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+
+var btn = document.getElementById('counter');
+btn.onlick = function() {
+    
+    
+    counter = counter + 1;
+    var count = document.getElementById('count');
+    count.innerHTML = count.toString();
+};
