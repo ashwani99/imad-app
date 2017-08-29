1d5aae09dc6a1e34ece17e03e54ca8bc06c55e8d
Ashwani Gupta <ashwani99@users.noreply.github.com>
2017-08-30 03:17:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,21 @@ console.log('Loaded!');
 var btn = document.getElementById('counter');
 btn.onlick = function() {
     
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }    
+        // Not done yet
+    };
     
     counter = counter + 1;
     var count = document.getElementById('count');
